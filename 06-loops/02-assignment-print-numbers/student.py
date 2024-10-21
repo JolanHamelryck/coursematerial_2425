d8ddb76e5a021694c9aafaf1ce5ba1aac0f9cada
Jolan <jolan.hamelryck@student.ucll.be>
2024-10-21 19:39:33 +0200

    hm

--- 06-loops/02-assignment-print-numbers/student.py
+++ 06-loops/02-assignment-print-numbers/student.py
@@ -1,7 +1,7 @@
 # write your code here
 def print_numbers(start, stop, step):
     i = start
-    while i != stop:
+    while i < stop:
         print(i)
         i += step
-    
+        
--- 06-loops/03-assignment-thanos/student.py
+++ 06-loops/03-assignment-thanos/student.py
@@ -1,1 +1,7 @@
 # write your code here
+def function(string):
+       if len(string) != 0:
+            return string[len(string) - 1]
+       else:
+            return None
+print(function("AZERTY"))
